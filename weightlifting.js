1af60f75e5fc2aa9a8896eb50f1966a71f5bff2c
Maria B <mariab@Marias-MacBook-Pro.local>
2019-07-11 15:01:38 -0700

    map

--- weightlifting.js
+++ weightlifting.js
@@ -18,3 +18,15 @@ function openCity(evt, cityName) {
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
 }
+var map = new ol.Map({
+  target: 'map',
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM()
+    })
+  ],
+  view: new ol.View({
+    center: ol.proj.fromLonLat([37.41, 8.82]),
+    zoom: 4
+  })
+});
